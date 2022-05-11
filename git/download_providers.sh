e745ef386d3c59588527ea4c4f24b08c822529d4
Andris <andris@MacBook-Air-von-Andris.local>
2022-05-11 16:03:39 +0200

    initial commit

---
+++ git/download_providers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PLUGIN_VERSION=2.11.0
+
+wget https://releases.hashicorp.com/terraform-provider-kubernetes/${PLUGIN_VERSION}/terraform-provider-kubernetes_${PLUGIN_VERSION}_linux_amd64.zip
+
