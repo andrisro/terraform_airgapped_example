2fa5268138ee5be24dd981ba2cc19283a783c4eb
Andris <andris@MacBook-Air-von-Andris.local>
2022-05-13 15:33:57 +0200

    added commands to move plugin to target directory structure

--- git/download_providers.sh
+++ git/download_providers.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
-
+# set plugin version
 export PLUGIN_VERSION=2.11.0
 
-wget https://releases.hashicorp.com/terraform-provider-kubernetes/${PLUGIN_VERSION}/terraform-provider-kubernetes_${PLUGIN_VERSION}_linux_amd64.zip
+export PLUGIN_FILE_NAME=terraform-provider-kubernetes_${PLUGIN_VERSION}_linux_amd64.zip
 
+# download terraform kubernetes provider
+wget https://releases.hashicorp.com/terraform-provider-kubernetes/${PLUGIN_VERSION}/${PLUGIN_FILE_NAME}
+
+# move to directory
+export FILEPATH=/providers_source/registry.terraform.io/hashicorp/kubernetes/${PLUGIN_VERSION}/
+mkdir -p ./providers_source/registry.terraform.io/hashicorp/kubernetes/${PLUGIN_VERSION}/
+
+# move plugin to target directory
+cp ${PLUGIN_FILE_NAME} ./${FILEPATH}/${PLUGIN_FILE_NAME}
